180bd532979738298ef050931b32722bbbb7879f
Axemasta <33064621+Axemasta@users.noreply.github.com>
2020-12-02 11:53:57 +0000

    Pro tabs now with fonts
    If you include the pro fonts in the resources the tabs will be loaded, the assembly info exports will still need to be manually commented back in

--- src/FontAwesomeForms/App.xaml.cs
+++ src/FontAwesomeForms/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using FontAwesomeForms.Models;
 using FontAwesomeForms.Pages;
 using FontAwesomeForms.ViewModels;
@@ -45,14 +48,16 @@ namespace FontAwesomeForms
                 codeFree
             };
 
-            //Pro - Uncomment if you installed pro fonts :)
-            var proViewModel = new ProFontsViewModel();
+            if (ProFontsLoaded())
+            {
+                var proViewModel = new ProFontsViewModel();
 
-            var xamlPro = new XamlPage(proViewModel);
-            var codePro = new CodePage(proViewModel);
+                var xamlPro = new XamlPage(proViewModel);
+                var codePro = new CodePage(proViewModel);
 
-            pages.Add(xamlPro);
-            pages.Add(codePro);
+                pages.Add(xamlPro);
+                pages.Add(codePro);
+            }            
 
             pages.Add(new MiscPage());
 
@@ -76,6 +81,15 @@ namespace FontAwesomeForms
             tabbed.On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
 
             return tabbed;
+        }
+
+        bool ProFontsLoaded()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var resources = assembly.GetManifestResourceNames();
+
+            return resources.Any(r => Regex.IsMatch(r, "(?i)pro"));
         }
 
         protected override void OnStart()
--- src/FontAwesomeForms/AssemblyInfo.cs
+++ src/FontAwesomeForms/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using Xamarin.Forms.Xaml;
 [assembly: ExportFont("FontAwesome5FreeSolid.otf", Alias = "FAFreeSolid")]
 
 //Pro Fonts
-[assembly: ExportFont("FontAwesome5ProBrands.otf", Alias = "FAProBrands")]
-[assembly: ExportFont("FontAwesome5ProDuotone.otf", Alias = "FAProDuoTone")]
-[assembly: ExportFont("FontAwesome5ProLight.otf", Alias = "FAProLight")]
-[assembly: ExportFont("FontAwesome5ProRegular.otf", Alias = "FAProRegular")]
-[assembly: ExportFont("FontAwesome5ProSolid.otf", Alias = "FAProSolid")]
+//[assembly: ExportFont("FontAwesome5ProBrands.otf", Alias = "FAProBrands")]
+//[assembly: ExportFont("FontAwesome5ProDuotone.otf", Alias = "FAProDuoTone")]
+//[assembly: ExportFont("FontAwesome5ProLight.otf", Alias = "FAProLight")]
+//[assembly: ExportFont("FontAwesome5ProRegular.otf", Alias = "FAProRegular")]
+//[assembly: ExportFont("FontAwesome5ProSolid.otf", Alias = "FAProSolid")]
