902998a5fceeebc85be1737bada0c04a9ed4047f
Axemasta <33064621+Axemasta@users.noreply.github.com>
2020-12-02 09:49:41 +0000

    Added Lightswitch
    Added Lightswitch NuGet so I can test the app in light / dark mode quickly

--- src/FontAwesomeForms.Android/MainActivity.cs
+++ src/FontAwesomeForms.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using LightSwitch;
 
 namespace FontAwesomeForms.Droid
 {
@@ -21,6 +22,11 @@ namespace FontAwesomeForms.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+#if DEBUG
+            LightSwitchAgent.Init();
+#endif
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
